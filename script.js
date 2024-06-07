76457568b077ff0c75126e258a2c2cd9e37c0caf
miguelkapicius <miguelkapicius@gmail.com>
2024-06-07 11:39:56 -0300

    fix: login

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-const buttonSubmit = document.querySelector('button[type="submit"]')
-
+const buttonSubmit = document.querySelector('button[type="submit"]');
 const user = "globalsolution@fiap.com.br";
 const correctPassword = "1234";
 
-buttonSubmit.addEventListener('click', () => validate())
+buttonSubmit.addEventListener("click", () => validate());
 
 function validate() {
     if (email.value === user && password.value === correctPassword) {
-        alert("Login válidado com sucesso.");
+        alert("Login efetuado com sucesso.");
         window.location.href = "/GS-LANDINGPAGE/";
     } else {
-        alert("Usuário ou senha incorretos!")
+        alert("Usuário ou senha incorretos!");
     }
 }
 
-function showSidebar(){
-    const sidebar = document.querySelector(".sidebar")
-    sidebar.style.display = "flex"
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+
+    sidebar.style.display = "flex";
 }
 
-function hideSidebar(){
-    const sidebar = document.querySelector(".sidebar")
-    sidebar.style.display = "none"
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+
+    sidebar.style.display = "none";
 }

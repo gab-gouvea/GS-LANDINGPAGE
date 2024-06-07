25dbf8d8caaac432a4c2366a7b8e50f236cc645f
Gabriel <oliveirag396@gmail.com>
2024-06-07 00:38:35 -0300

    fix: href login to index

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-const form = document.querySelector('form');
+const buttonSubmit = document.querySelector('button[type="submit"]')
 
 const user = "globalsolution@fiap.com.br";
 const correctPassword = "1234";
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); 
-    validate();
-});
+buttonSubmit.addEventListener('click', () => validate())
 
 function validate() {
     if (email.value === user && password.value === correctPassword) {
         alert("Login válidado com sucesso.");
-        window.location.href = "index.html";  
+        window.location.href = "/GS-LANDINGPAGE/index.html";
     } else {
-        alert("Usuário ou senha incorretos!");
+        alert("Usuário ou senha incorretos!")
     }
 }
-
 
 function showSidebar(){
     const sidebar = document.querySelector(".sidebar")
